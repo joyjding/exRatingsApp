f5de9f986544d2546b228d04f1177016f033854c
Hackbright Student <kate.m.odell@gmail.com>
2013-07-15 13:04:58 -0700

    initial commit

--- seed.py
+++ seed.py
@@ -2,8 +2,16 @@ import model
 import csv
 
 def load_users(session):
-    # use u.user
-    pass
+    filename = "seed_data/u.user"
+    with open(filename) as csvfile:
+        data = csv.reader(csvfile, delimiter="|")
+        for row in data:
+            print row
+            new_user = model.User(id=row[0], age=row[1], zipcode=row[4])
+            session.add(new_user)
+    session.commit()
+
+
 
 def load_movies(session):
     # use u.item
@@ -15,7 +23,7 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
 
 if __name__ == "__main__":
     s= model.connect()
